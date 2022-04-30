70b5fcec0071f27c7d4ac2f44fdd39f9e1ee82c0
Trouble-Andrew <satyr663@gmail.com>
2022-04-30 11:36:39 +0300

    feat: implement simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,26 +4,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
-const chainMaker = {
+ const chainMaker = {
+  result: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.result.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value = '') {
+    this.result.push(`( ${value} )`);
+    return chainMaker;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (typeof position === 'number' && position > 0 &&  position <= this.result.length) {
+      this.result.splice(position - 1, 1);
+      return chainMaker;
+    } else {
+      this.result = [];
+      throw new Error(`You can\'t remove incorrect link!`);
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.result.reverse();
+    return chainMaker;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const result = this.result.join('~~');
+    this.result = [];
+    return result;
   }
 };
 
